7a1b64c2a40d29d4f6357ae8414805fdc7505274
2kSHIVAM <shivamsinghrajput1357@gmail.com>
2025-09-13 13:01:58 +0530

    service: method added to fetch all buildings

--- backend/src/main/java/com/apartment_building_task/backend/service/BuildingService.java
+++ backend/src/main/java/com/apartment_building_task/backend/service/BuildingService.java
@@ -2,9 +2,12 @@ package com.apartment_building_task.backend.service;
 
 import com.apartment_building_task.backend.model.*;
 
+import java.util.List;
+
 public interface BuildingService {
     Building createBuilding(String id, double requestedTemperature);
     Building getBuilding(String id);
+    List<Building> getAllBuildings();
     void setRequestedTemperature(String buildingId, double temp);
     void addApartment(String buildingId, Apartment apartment);
     void addCommonRoom(String buildingId, CommonRoom room);
